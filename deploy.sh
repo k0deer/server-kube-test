119b640a5885e1fcea1156593f21905233bbf60d
raul <rlopezaldana@gmail.com>
2020-04-25 16:04:51 -0500

    config clean app

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ echo 'Delete old deploy kubernetes ... '
 
 #delete old deploy
 kubectl delete deployment clientes-v1 
-kubectl delete svc clientes 
+kubectl delete svc clientes-svc
 
 echo 'Successfully deleted ' 
 
--- kube/clean.sh
+++ kube/clean.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 kubectl delete deployment clientes-v1 
-kubectl delete svc clientes 
+kubectl delete svc clientes-svc
